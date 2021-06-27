28f1c8a05424ff0ba82659c5744586322435e0af
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2021-06-27 10:31:10 -0500

    adding count traking to endpoint by frontend

--- app/controllers/alerts_its_a_trap_controller.rb
+++ app/controllers/alerts_its_a_trap_controller.rb
@@ -4,7 +4,8 @@ class AlertsItsATrapController < ApplicationController
     render json: { position: 0,
                   ship: "Bomber",
                   value: 100,
-                  time_tracking: "2021-06-25 21:21:34" }
+                  time_tracking: "2021-06-25 21:21:34",
+                  count_tracker: 29}
   end
 
 end
