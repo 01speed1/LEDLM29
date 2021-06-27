0a75a3af568c439fff74d6d269b620ec4a9212c0
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2021-06-27 12:55:24 -0500

    create service to front

--- app/services/alerts_its_a_trap_service.rb
+++ app/services/alerts_its_a_trap_service.rb
@@ -1,2 +1,20 @@
-class AlertsItsATrapService
+module AlertsItsATrapService
+  def self.call
+    alert_sensor = SensorCounter.joins(sensor: :sensor_tracks)
+                    .where("count >= 30")
+                    .pluck('"sensors"."position"',
+                           '"sensor_tracks"."ship"',
+                           '"sensor_tracks"."value"',
+                           '"sensor_tracks"."time_tracking"',
+                           '"sensor_counters"."count"')
+
+    alert_sensor.map do |object|
+      { position: object[0],
+        ship: object[1],
+        value: object[2],
+        time_tracking: object[3].iso8601,
+        count_tracker: object[4]
+      }
+    end
+  end
 end
